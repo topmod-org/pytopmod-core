6c1e2fb8d4a25aad6ac0f8194a66460d50ae4727
davyrisso <davyrisso@gmail.com>
2023-12-15 13:02:19 +0100

    Implement common base types (mesh, face, vertex, etc.)

---
+++ src/pytopmod/core/face.py
@@ -0,0 +1,4 @@
+from typing import TypeAlias
+
+# Type alias for a face key.
+FaceKey: TypeAlias = str
---
+++ src/pytopmod/core/vertex.py
@@ -0,0 +1,4 @@
+from typing import TypeAlias
+
+# Type alias for a vertex key.
+VertexKey: TypeAlias = str
